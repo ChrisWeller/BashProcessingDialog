9232f2194ff44b9638b82889503ea3899cbbd684
Chris Weller <chris.weller@trunknetworks.com>
2015-06-04 14:07:25 +0100

    Added running of x commands

---
+++ run_x_commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+(
+NumCommands=${#Commands[@]}
+echo "Num Commands $NumCommands"
+for i in "${!Commands[@]}"; do 
+	Percentage=$((i * 100 / NumCommands))
+	echo $Percentage
+	echo "# ${Notes[$i]}"
+	${Commands[$i]}
+done
+) |
+zenity --progress --title="Run SVN update on IMP" --text="Starting..."  --percentage=0 --auto-close --no-cancel --width=300
