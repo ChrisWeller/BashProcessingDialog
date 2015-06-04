474c649e3d209e0f14a7214afce23db49b2605bb
Chris Weller <chris.weller@trunknetworks.com>
2015-06-04 14:29:35 +0100

    Update to readme and correct path entries

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # BashProcessingDialog
 Scripts to show dialogs during bash scripts
 
+```Shell
 Commands[0]="echo Enter your command here"
 Commands[1]="echo Followed by another command if have some"
 
@@ -8,3 +9,4 @@ Notes[0]="The text of the dialog goes here"
 Notes[1]="One line for each command"
 
 source $(dirname $0)/run_commands.sh
+```
--- run_commands.sh
+++ run_commands.sh
@@ -2,13 +2,14 @@
 
 if ! xset q &>/dev/null; then
 
-    for i in "${!Commands[@]}"; do 
-    	Percentage=$((i * 100 / NumCommands))
-    	echo $Percentage
-    	echo "# ${Notes[$i]}"
-    	${Commands[$i]}
-    done
+	for i in "${!Commands[@]}"; do 
+		Percentage=$((i * 100 / NumCommands))
+		echo $Percentage
+		echo "# ${Notes[$i]}"
+		${Commands[$i]}
+	done
 
 else
-    source $(dirname $0)/run_x_commands.sh
+	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	source $DIR/run_x_commands.sh
 fi
